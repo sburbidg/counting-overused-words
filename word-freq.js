a3e306c87db37f50a2e8233b769efaedc8c14ee8
Scott Burbidge <scottisreally@gmail.com>
2014-11-25 10:26:38 -0800

    minor syntax changes

--- word-freq.js
+++ word-freq.js
@@ -9,27 +9,23 @@ var S = require('string');
 //output: warning of overused words
 
 fs.readFile(process.argv[2], function(error, data){
-   var pageAsString = data.toString();
-   var noPunct = S(pageAsString).stripPunctuation().s;
-   var noWhiteSpace = S(noPunct).collapseWhitespace().s;
-   var wordArray =noWhiteSpace.split(" ");
+  var pageAsString = data.toString();
+  var noPunct = S(pageAsString).stripPunctuation().s;
+  var noWhiteSpace = S(noPunct).collapseWhitespace().s;
+  var wordArray =noWhiteSpace.split(" ");
 
    //calcuates frequency of each word
-   var freq = word.freq(pageAsString, true, false);
+  var freq = word.freq(pageAsString, true, false);
   var numWord = wordArray.length;
 
   var overusedPercentage = 0.01 * process.argv[3];
-  //console.log(freq);
 
   for (var i in freq) {
     if (freq[i]/numWord > overusedPercentage){
-          console.log("Warning! You overused the word \"" + i + "\" (" + freq[i] +
+      console.log("Warning! You overused the word \"" + i + "\" (" + freq[i] +
         " times in a  " + numWord + " word long document.)");
     }
   }
-   //console.log("The word \"" + stringSearch + "\" occurs " +
-    //freq[stringSearch] + " times.");
-    //console.log(pageAsString.split(" "));
 });
 
 
